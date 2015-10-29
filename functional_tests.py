08ab47ef933d3f927393310ac37497729c7c69da
ubracklow <ubracklow@hotmail.com>
2015-10-29 17:49:05 +0100

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,41 +1,51 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
-	
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		# Edith has heard about a cool new online to-do app. She goes
-		# to check out its homepage
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
 
-		self.browser.get('http://localhost:8000')
+        # she notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-		# she notices the page title and header mention to-do lists
-		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
 
-		# She is invited to enter a to-do item straight away
+        # she types 'Buy peacock feathers' into a box (Edith's hobby 
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-if __name__ == '__main__':
-		unittest.main(warnings='ignore')
+        # When she hits enter, the page updates, and now the page lists 
+        # '1: Buy peacock feathers' as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
+        # There is still a text box inviting her to ass another item. She 
+        # enters 'Use peacock feathers to make a fly' 
 
-
-# she types 'Buy peacock feathers' into a box (Edith's hobby 
-# is tying fly-fishing lures)
-
-# When she hits enter, the page updates, and now the page lists 
-# '1: Buy peacock feathers' as an item in a to-do list
-
-# There is still a text box inviting her to ass another item. She 
-# enters 'Use peacock feathers to make a fly' 
+        self.fail('Finish the test!')
 
 # The page updates again, and now shows both items on her list
 
@@ -46,6 +56,9 @@ if __name__ == '__main__':
 # She visits that URL - her to-do list is still there.
 
 # Satisfied, she goes back to sleep.
+
+if __name__ == '__main__':
+        unittest.main(warnings='ignore')
 
 browser.quit()
 
